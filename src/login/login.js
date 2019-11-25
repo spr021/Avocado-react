2ab1f760d4c5b9536b53c033323ee2b37552cf0e
saber <saber.pourrahimi.1999@gmail.com>
2019-11-25 21:16:38 +0330

    complet sign in desgine

--- src/login/login.js
+++ src/login/login.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import logo from '../login/logo2.png'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 import {
   BrowserRouter as Router, Link
 } from 'react-router-dom'
@@ -15,16 +17,16 @@ class Login extends React.Component {
             <p>Please enter your email address and password.</p>
           </div>
           <div>
-            <input className='email' type='email' placeholder='E-mail' />
+            <TextField className='email-login' id='outlined-required' label='E-mail' variant='outlined' />
           </div>
           <div>
-            <input className='password' type='password' placeholder='Password' />
+            <TextField className='password-login' id='outlined-password-input' label='Password' type='password' autoComplete='current-password' variant='outlined' />
           </div>
           <div className='signInButton'>
-            <Link className='signInLink' to='/im/'>Sign in</Link>
+            <Link to='/im/'><Button className='button-login' variant='contained' color='primary'>Sing in</Button></Link>
             <div className='forgotText'>
               <Link className='forgotPassword' to='/forgot-password/'>Forgot Password?</Link>
-              <div>Don't have an account?<Link className='signUpLink' to='/sign-up/'>Sign up</Link></div>
+              <div>Don't have an account?<Link className='signUpLink' to='/sign-up/'> Sign up</Link></div>
             </div>
           </div>
         </div>
